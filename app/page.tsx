7afa5a79c25c8687f743e7b334e139fc43b93cfd
adnansami279 <adnan.sami@forbmax.ai>
2025-12-10 18:31:05 +0500

    Remove ReactHookForm component import and usage from the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import ReactHookForm from "./ReactHookForm";
 import Table from "./Table";
 
 export default function Home() {
   return (
     <div>
       <Table />
-      {/* <ReactHookForm /> */}
     </div>
   );
 }
